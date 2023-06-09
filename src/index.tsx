7f42782b6be500ebb93cb4fe21eef8925df6a5b0
xsf0105 <allan91coder@gmail.com>
2023-06-09 15:11:50 +0800

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,320 @@
-import { QuarkElement, Fragment, property, customElement } from "quarkc";
-import style from "./index.less?inline";
+import { QuarkElement, Fragment, state, property, customElement } from "quarkc"
+import style from "./index.less?inline"
+import langs from "./lang"
+import quarkLogo from "./images/quark-logo.png"
+import arrow2 from "./images/arrow2.png"
+import arrowLight from "./images/arrow-light.png"
+import iDownLight from "./images/i-down-light.png"
+import iDown from "./images/i-down.png"
 
 declare global {
   interface HTMLElementTagNameMap {
-    "my-component": MyComponent;
+    "quark-doc-home": QuarkDocHome;
   }
 }
 
-@customElement({ tag: "my-component", style })
-class MyComponent extends QuarkElement {
-  @property({ type: Number }) // 外部属性
-  count = 0;
+@customElement({ tag: "quark-doc-home", style })
+export default class QuarkDocHome extends QuarkElement {
+  #isZhLang
+  #ecosystemLangs
+  #framework
 
-  @property({ type: String })
-  text = '';
+  constructor() {
+    super()
+    this.#ecosystemLangs = this.#isZhLang ? langs["zh-CN"] : langs["en-US"]
+    this.#framework = ["Vue2.x", "Vue3.x", "React", "Angular", "Svelte", "Vanilla"]
+  }
 
-  add = () => {
-    // 内部事件
-    this.count += 1;
-  };
+  @state()
+  darkMode = false
+
+  @state()
+  activeFwIndex = 0
+
+  @state()
+  timeInter = null
 
   componentDidMount() {
-    // 生命周期
-    console.log("dom loaded!");
-    // ...
+    if(localStorage.getItem('theme') === 'dark') {
+      this.darkMode = true;
+    }
+
+    this.timeInter = setInterval(() => {
+      if (this.activeFwIndex >= 5) {
+        this.activeFwIndex = 0;
+      } else {
+        this.activeFwIndex ++;
+      }
+    }, 3000);
+
+    console.log(this.shadowRoot.querySelector("#we-believe"), this.shadowRoot.querySelector("#scroll-down"), 22);
+
+    const scrollDown = this.shadowRoot.querySelector("#scroll-down")
+
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        console.log(entries, 989);
+
+        // 进入视窗
+        if (entries[0].isIntersecting) {
+          console.log(1);
+
+          scrollDown.style.opacity = 0;
+        } else {
+          console.log(2);
+          scrollDown.style.opacity = 1;
+        }
+      }
+    )
+    intersectionObserver.observe(this.shadowRoot.querySelector("#we-believe"));
+
+
+    // this.activeFwIndex = new Proxy({}, {
+    //   get: function(target, name){
+    //     console.log(target, name, 111);
+    //     this.shadowRoot.querySelector(".tech-name").style.animation =
+    //     "3s infinite text-alter";
+
+    //     return target[name]
+    //   },
+    //   set: function(target, prop, value, receiver) {
+    //     console.log(target, prop, value, receiver, 2222);
+    //     // target[prop] = value;
+    //     // console.log('property set: ' + prop + ' = ' + value);
+    //     return true;
+    //   }
+    // })
+
   }
 
   render() {
     return (
       <Fragment>
-        <div>
-          <a href="https://quark.hellobike.com" target="_blank">
-            <img
-              src="https://quark-design.hellobike.com/assets/quark-logo.f9a6a307.png"
-              class="logo"
-              alt="quark logo"
-            />
-          </a>
-        </div>
+        <main class="home-main">
+          <section class="home-section">
+              <div class="home-text">
+                <div>
+                  <div class="home-logo">
+                    <img src={quarkLogo} alt="" />
+                  </div>
+                  <h1 class="home-title">
+                    { this.#ecosystemLangs.homeTitle }
+                  </h1>
+                  <p class="home-subtitle2 text-grad">
+                    { this.#ecosystemLangs.homeSubtitle2 }
+                    &nbsp;
+                    <span class="tech-name text-grad">{ this.#framework[this.activeFwIndex] }</span>
+                  </p>
 
-        <h1>Quark - {this.text}</h1>
+                  <div class="actions">
+                    <div class="action">
+                      <a
+                        class="get-started"
+                        href={ this.#isZhLang ? `#/zh-CN/guide/quickstart`: `#/en-US/guide/quickstart`}
+                        >
+                          { this.#ecosystemLangs.getStarted }
+                      </a>
+                    </div>
+                    <div class="action">
+                      <a
+                        class="why-quark"
+                        href={this.#isZhLang ? `#/zh-CN/guide/introduction` : `#/en-US/guide/introduction`}
+                        >
+                          { this.#ecosystemLangs.whyQuark }
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a
+                class="scroll-down"
+                id="scroll-down"
+                href="#we-believe"
+                style="opacity: 1"
+              >
+                <span>{ this.#ecosystemLangs.scrollDown }</span>
+                <span class="arrow">
+                  {
+                    this.darkMode ?
+                    <img
+                      src={arrow2}
+                      alt="arrow"
+                    /> :
+                    <img src={arrowLight} alt="arrow" />
+                  }
+                </span>
+              </a>
+            </section>
 
-        <div className="card">
-          <button onClick={this.add}>count is: {this.count}</button>
+            <section class="we-believe" id="we-believe">
+              <div class="wrap">
+                <h2>
+                  { this.#ecosystemLangs.weBelieve }
+
+                  {
+                    this.darkMode ?
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDown}
+                      height="34"
+                      width="36"
+                    />:
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDownLight}
+                      height="34"
+                      width="36"
+                    />
+                  }
+                </h2>
+                <p>
+                  { this.#ecosystemLangs.weBelieveDesc1 }
+                </p>
+                <p style="margin-top: 1.5rem">
+                  { this.#ecosystemLangs.weBelieveDesc2 }
+                </p>
+                <p style="margin-top: 1.5rem">
+                  { this.#ecosystemLangs.weBelieveDesc3 }
+                </p>
+              </div>
+            </section>
+
+            <section class="what-we-do" id="what-we-do">
+              <div class="wrap">
+                <h2>
+                  { this.#ecosystemLangs.whatWeDo }
+                  {
+                    this.darkMode ?
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDown}
+                      height="34"
+                      width="36"
+                    /> :
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDownLight}
+                      height="34"
+                      width="36"
+                    />
+                  }
+
+                </h2>
+                <p>
+                  { this.#ecosystemLangs.whatWeDoDesc1 }
+                </p>
+                <p style="margin-top: 1.5rem">
+                  { this.#ecosystemLangs.whatWeDoDesc2 }
+                </p>
+
+                {/* <div class="code-demo-tab-group">
+                  <button
+                    v-for="item in tabs"
+                    :key="item"
+                    @click="handleTabSwitch(item)"
+                    :class="activeTab === item ? 'active-tab' : ''"
+                  >
+                    { item }
+                  </button>
+                </div>
+
+                <div class="code-demo-container">
+                  <div class="code-demo relative">
+                    <div>
+                      <div class="editor-skin-header">
+                        <div class="editor-skin-header-btn-group">
+                          <span class="editor-skin-header-btn red"></span>
+                          <span class="editor-skin-header-btn yellow"></span>
+                          <span class="editor-skin-header-btn green"></span>
+                        </div>
+                      </div>
+                      <CodeDemo :tabName="activeTab" />
+                    </div>
+
+                    <div class="preview">
+                      <quark-button size="big" :loading="isLoading" @click="handleClick"
+                        >Button</quark-button
+                      >
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </section>
+
+            {/* <section class="advantage" id="advantage">
+              <div class="wrap">
+                <h2>
+                  { this.#ecosystemLangs.advantage }
+                  {
+                    this.darkMode ?
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDown}
+                      height="34"
+                      width="36"
+                    />
+                    :
+                    <img
+                      class="down-icon"
+                      alt="down-icon"
+                      src={iDownLight}
+                      height="34"
+                      width="36"
+                    />
+                  }
+                </h2>
+                <div class="box-group">
+                  <div class="box1">
+                    <h2>{ this.#ecosystemLangs.box1Title }</h2>
+                    <p>{ this.#ecosystemLangs.box1Desc }</p>
+                  </div>
+                  <div class="box2">
+                    <h2>{ this.#ecosystemLangs.box2Title }</h2>
+                    <p>{ this.#ecosystemLangs.box2Desc }</p>
+                  </div>
+                  <div class="box3">
+                    <h2>{ this.#ecosystemLangs.box3Title }</h2>
+                    <p>{ this.#ecosystemLangs.box3Desc }</p>
+                  </div>
+                  <div class="what">
+                    <p>{ this.#ecosystemLangs.whatDesc1 }</p>
+                    <p>{ this.#ecosystemLangs.whatDesc2 }</p>
+                    <p>
+                      <a href="https://github.com/hellof2e/quark-cli" target="_blank"
+                        >阅读此篇</a
+                      >
+                      了解更多信息。
+                    </p>
+                  </div>
+                  <div class="how">
+                    <div class="bash-code">
+                      <pre class="language-bash">
+        npx create-quark-app create project-name
+        cd project-name
+
+        npm install
+        npm start
+                      </pre>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section> */}
+          </main>
+
+        <div class="footer-container">
+          <footer>
+            <div class="text-xs text-gray-400">
+              A project by <a href="/"> Quark Labs</a>
+            </div>
+          </footer>
         </div>
       </Fragment>
     );
   }
 }
-
-export default MyComponent;
---
+++ src/lang.ts
@@ -0,0 +1,66 @@
+export default {
+  "zh-CN": {
+    homeTitle: "下一代浏览器原生组件库",
+    homeSubtitle:
+      "它可以同时在任意框架或无框架中使用（跨技术栈组件库、无框架组件库）",
+    homeSubtitle2: "它可以被运行在",
+    getStarted: "开始",
+    whyQuark: "为什么选择 Quark？",
+    scrollDown: "Scroll Down",
+    weBelieve: "我们认为",
+    weBelieveDesc1: "组件库可以不用依赖技术框架而存在。",
+    weBelieveDesc2: "如果能做到这点，那么这个组件库可以穿越沉浮，历经长久!",
+    weBelieveDesc3: "不用随着技术栈的更新而更新。",
+    whatWeDo: "我们做了什么",
+    whatWeDoDesc1:
+      "得益于 Web Components 在浏览器的原生支持特性，我们使用该技术开发了 Quark Design。",
+    whatWeDoDesc2: "这让前端组件库真正实现了「技术栈无关」。",
+    advantage: "有什么优势",
+    box1Title: "减少代码量",
+    box1Desc:
+      "构建的组件无需依赖第三方 runtime，平均每个组件体积 1kb ~ 2kb 左右。",
+    box2Title: "浏览器原生",
+    box2Desc: "依托于 Web components 技术，quarkd 可以被任何前端框架使用。",
+    box3Title: "版本稳定",
+    box3Desc:
+      "不同于其它第三方组件库，quarkd 不会随着技术栈的升级而更新迭代，无需常年更新，也没有历史包袱",
+    whatDesc1:
+      "Quarkd 是一种全新的组件构建方式。传统组件框架如 Antd 和 Vant 等在浏览器中需要做大量的工作，而 Quark 将这些工作放到构建应用程序的编译阶段来处理。",
+    whatDesc2: "组件的数据修改由浏览器自身 API 处理，更简单，更高效！",
+  },
+  "en-US": {
+    homeTitle: "Next-gen browser-native component library",
+    homeSubtitle:
+      "It can be used in any frame or frameless at the same time (ps: frameless component library)",
+    homeSubtitle2: "It can be run on",
+    getStarted: "Get Started",
+    whyQuark: "Why Quark?",
+    scrollDown: "Scroll Down",
+    weBelieve: "WE BELIEVE",
+    weBelieveDesc1:
+      "The component library can exist without relying on the technical framework.",
+    weBelieveDesc2:
+      "If this can be done, then the component library can go through ups anddowns and go through a long period of time!",
+    weBelieveDesc3:
+      "It does not need to be updated with the update of the technology stack.",
+    whatWeDo: "WHAT WE DO",
+    whatWeDoDesc1:
+      "Benefiting from the native support of web components in browsers, we developed Quark Design using this technology.",
+    whatWeDoDesc2:
+      "This makes the front-end component library truly independent of the technology stack.",
+    advantage: "Advantages",
+    box1Title: "Write less code",
+    box1Desc:
+      "The built components do not need to rely on third-party runtimes, and the average volume of each component is about 1kb ~ 2kb.",
+    box2Title: "Browser native",
+    box2Desc:
+      "Relying on Web components technology, quarkd can be used by any front-end framework.",
+    box3Title: "Stable version",
+    box3Desc:
+      "Unlike other third-party component libraries, quarkd will not update and iterate with the upgrade of the technology stack. It does not need to be updated all year round, and there is no historical burden.",
+    whatDesc1:
+      "Quarkd is a brand new way of building components. Traditional component frameworks such as Antd and Vant need to do a lot of work in the browser, but Quark puts these tasks into the compilation phase of building applications.",
+    whatDesc2:
+      "The data modification of the component is handled by the browser's own API, which is simpler and more efficient!",
+  },
+};
